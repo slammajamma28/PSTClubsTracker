1de8a2f3caabc2907aa19bc72caa07e981f76bb5
Slamma <slammajamma28@gmail.com>
2020-02-11 16:31:44 -0500

    Added up to Byrney
    Also changed icons to be static variable so I don't need to copy img/name.png each time

--- js/lists.js
+++ js/lists.js
@@ -1,9 +1,9 @@
 /* Custom Clubs Array */
-//var sub_icon="img/subscriber.png"
-//var motm_icon="img/plustom.png"
+var sub_icon="img/subscriber.png"
+var motm_icon="img/plustom.png"
 var lists = [{
     listName: "Aeirou's Unlikely Heroes Subscriber Club",
-    award: "img/subscriber.png",
+    award: sub_icon,
     games: [{
         game: "Assassin's Creed II",
         imag: "https://www.playstationtrophies.org/images/trophies/260/ico.png"
@@ -22,7 +22,7 @@ var lists = [{
     }]
 },{
     listName: "Aeliana's Fabula Nova Crystallis MotM Club",
-    award: "img/plustom.png",
+    award: motm_icon,
     games: [{
         game: "Final Fantasy XIII",
         imag: "https://www.playstationtrophies.org/images/trophies/307/ico.png"
@@ -38,7 +38,7 @@ var lists = [{
     }]
 },{
     listName: "Aeliana's Vocaloid Synesthesia Subscriber Club",
-    award: "img/subscriber.png",
+    award: sub_icon,
     games: [{
         game: "Hatsune Miku: Project Diva f",
         imag: "https://www.playstationtrophies.org/images/trophies/1695/ico.png"
@@ -54,7 +54,7 @@ var lists = [{
     }]
 },{
     listName: "Alex Fly's Best Games of My Favorite Franchises MotM Club",
-    award: "img/plustom.png",
+    award: motm_icon,
     games: [{
         game: "Assassin's Creed Origins",
         imag: "https://www.playstationtrophies.org/images/trophies/5865/ico.png"
@@ -73,7 +73,7 @@ var lists = [{
     }]
 },{
     listName: "AmbaLaBamba's Back Off Man, I'm a Scientist Subscriber Club",
-    award: "img/subscriber.png",
+    award: sub_icon,
     blurb: "As many of you know, I'm a huge Ghostbusters fan, and this is how this club came about. Here is a selection of scientific-y games that I feel fit perfectly - something for everybody. Enjoy them and remember - who you gonna call?",
     games: [{
         game: "Assassin's Creed II",
@@ -93,7 +93,7 @@ var lists = [{
     }]
 },{
     listName: "AppleKratue's All About Art MotM Club",
-    award: "img/plustom.png",
+    award: motm_icon,
     blurb: "Videogames are art. While you are playing through these games, it may happen you are amidst impressive scenery. Take a deep breath and enjoy some of the finest art which is only possible with videogames.",
     games: [{
         game: "Castlevania: Lords of Shadow",
@@ -113,7 +113,7 @@ var lists = [{
     }]
 },{
     listName: "Ash's I'll Kill YOUR Zone! MotM Club",
-    award: "img/plustom.png",
+    award: motm_icon,
     blurb: "The good old Halo-killer is here in club form! What's that? It's not a Halo-killer? Well, I don't see Halo on PlayStation. Do you? Exactly.",
     games: [{
         game: "Killzone",
@@ -133,7 +133,7 @@ var lists = [{
     }]
 },{
     listName: "Ash's Opinions Be Damned These Games Are Great Go Play Them Subscriber Club",
-    award: "img/subscriber.png",
+    award: sub_icon,
     blurb: "Five great games across three great platforms. No excuses, go play them now!",
     games: [{
         game: "Oddworld: Abe's Oddysee - New 'n' Tasty",
@@ -153,7 +153,7 @@ var lists = [{
     }]
 },{
     listName: "Ashbo82's Absolute MotM Club Scenes!",
-    award: "img/plustom.png",
+    award: motm_icon,
     blurb: "I picked 5 different genres of games (sort of) I have completed & that I found really fun & rewarding to play & I think if you havent yet, then sign up & try them out too!!.",
     games: [{
         game: "Dishonored 2",
@@ -173,7 +173,7 @@ var lists = [{
     }]
 },{
     listName: "Beachhead's Basic Training Subscriber Club",
-    award: "img/subscriber.png",
+    award: sub_icon,
     blurb: "Aside from being my favorite games, none of these games are that difficult to platinum. Which is a good thing for you, grunt! After completing these games you will be ready to tackle any game thrown your way. So good luck and good hunting, grunts!",
     games: [{
         game: "Battlefield 4",
@@ -193,7 +193,7 @@ var lists = [{
     }]
 },{
     listName: "Bic's Favorites MotM Club",
-    award: "img/plustom.png",
+    award: motm_icon,
     blurb: "Nothing flash, Nothing crazy, Just simply my favourites to date - Probably in this correct order, Well worth the fun.",
     games: [{
         game: "Call of Duty: Black Ops",
@@ -211,10 +211,127 @@ var lists = [{
         game: "Uncharted 2: Among Thieves",
         imag: "https://www.playstationtrophies.org/images/trophies/214/ico.png"
     }]
-
+},{
+    listName: "Bic's We Go Again Subscriber Club",
+    award: sub_icon,
+    blurb: "Says it all in the title :)",
+    games: [{
+        game: "Borderlands 2",
+        imag: "http://www.ps3trophies.org/images/trophies/1145/ico.png"
+    },{
+        game: "Call of Duty: Modern Warfare 2",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00791_00_44468E11DB1E6DC3F7AC4728ED03C1278F8F23E8/2DA80D908EDA1F75165EB6402C88D5BAD82B0E6B.PNG"
+    },{
+        game: "Final Fantasy XIII-2",
+        imag: "http://www.ps3trophies.org/images/trophies/890/ico.png"
+    },{
+        game: "God of War II",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00951_00_F305A0C1BE62194F375365180B67B7B302DC5DB4/16CDB52BB73F4101A9C1826B143CFA7BA5987A57.PNG"
+    },{
+        game: "Saw II",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR01439_00_100FA709AA924CC2E9FEA695E997EA0FF03EA993/A06786C6D1F38BC5D5E474A2F7B7EE0A5DA3C400.PNG"
+    }]
+},{
+    listName: "Blaze Naruto Shippuden's I Don't Care Who I Have To Fight MotM Club",
+    award: motm_icon,
+    blurb: "Looking for a Naruto Themed Club to join? Then this is the club for you! Become the Perfect Storm Master by achieving the trophies from all 5 Naruto games currently available. Make the Seventh Hokage proud.",
+    games: [{
+        game: "Naruto Shippuden: Ultimate Ninja Storm 2",
+        imag: "https://i.imgur.com/aZ6OuRH.png"
+    },{
+        game: "Naruto Shippuden: Ultimate Ninja Storm 3",
+        imag: "https://www.playstationtrophies.org/images/trophies/1607/ico.png"
+    },{
+        game: "Naruto Shippuden: Ultimate Ninja Storm 4",
+        imag: "https://www.playstationtrophies.org/images/trophies/3306/ico.png"
+    },{
+        game: "Naruto Shippuden: Ultimate Ninja Storm Generations",
+        imag: "https://www.playstationtrophies.org/images/trophies/1121/ico.png"
+    },{
+        game: "Naruto Shippuden: Ultimate Ninja Storm Revolution",
+        imag: "https://www.playstationtrophies.org/images/trophies/2429/ico.png"
+    }]
+},{
+    listName: "Brazen's Games That Go Bump In The Night Subscriber Club",
+    award: sub_icon,
+    blurb: "I'm a big horror movie/games fan. These five games will guarantee you'll check your closet and under your bed at night.",
+    games: [{
+        game: "Dead Space 2",
+        imag: "http://www.ps3trophies.org/images/trophies/355/ico.png"
+    },{
+        game: "Resident Evil",
+        imag: "https://www.playstationtrophies.org/images/trophies/2957/ico.png"
+    },{
+        game: "The Evil Within",
+        imag: "https://www.playstationtrophies.org/images/trophies/3107/ico.png"
+    },{
+        game: "Until Dawn",
+        imag: "http://www.ps3trophies.org/images/trophies/1672/ico.png"
+    },{
+        game: "White Night",
+        imag: "https://www.playstationtrophies.org/images/trophies/3374/ico.png"
+    }]
+},{
+    listName: "Bread's Dat Story Tho Subscriber Club",
+    award: sub_icon,
+    blurb: "These games have, in my opinion, the best written storylines in recent video game history. They also share a common theme (kinda). Please play them now, and I promise you you won't regret it.",
+    games: [{
+        game: "Bioshock Infinite",
+        imag: "http://www.ps3trophies.org/images/trophies/700/ico.png"
+    },{
+        game: "Life is Strange",
+        imag: "https://www.playstationtrophies.org/images/trophies/2971/ico.png"
+    },{
+        game: "Steins;Gate",
+        imag: "https://www.playstationtrophies.org/images/trophies/3878/ico.png"
+    },{
+        game: "Zero Escape: Virtue's Last Reward",
+        imag: "https://www.playstationtrophies.org/images/trophies/1773/ico.png"
+    }]
+},{
+    listName: "Bucket's I Can't Use These Two Things Together Subscriber Club",
+    award: sub_icon,
+    blurb: "My reason for picking these games is kind of simple: I'm a fan of point-and-click adventure games and hidden object games and I wish more people would play some of the great (read: opinion) ones available for PlayStation players whether they come with platinums or just a 100% trophy list. It was difficult to limit the list to just these 5, but these are some of the ones I enjoyed the most and thus they form the list for this Subscriber Club.",
+    games: [{
+        game: "Broken Age",
+        imag: "https://www.playstationtrophies.org/images/trophies/3501/ico.png"
+    },{
+        game: "Grim Fandango",
+        imag: "https://www.playstationtrophies.org/images/trophies/2909/ico.png"
+    },{
+        game: "Red Johnson's Chronicles: One Against All",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR02662_00_301713CB6F7A67AA585E198A5292159403417B7E/D0DDBDE6BC6952AD83801B1AB12B0258EB739166.PNG"
+    },{
+        game: "Sam & Max: The Devil's Playhouse - Episode 1",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR01104_00_2903954A2CD8F7CFA4A071F31DA344A59AEBC008/3423884AA2A970FD7B2AA2E90D472FD4C47DACAC.PNG"
+    },{
+        game: "Syberia",
+        imag: "https://www.playstationtrophies.org/images/trophies/3165/ico.png"
+    }]
+},{
+    listName: "Byrney's Purple Dino Subscriber Club",
+    award: sub_icon,
+    blurb: "I love you, you love me, hugs for my PST family, sign up to Byrney's club and you deserve a clap, while you weren't looking I stole your hubcaps.",
+    games: [{
+        game: "Journey",
+        imag: "https://i.imgur.com/tPSsjCu.png"
+    },{
+        game: "LEGO Jurassic World",
+        imag: "https://www.playstationtrophies.org/images/trophies/3375/ico.png"
+    },{
+        game: "Middle Earth: Shadow of Mordor",
+        imag: "https://www.playstationtrophies.org/images/trophies/2378/ico.png"
+    },{
+        game: "Rocket League",
+        imag: "http://www.ps3trophies.org/images/trophies/3583/ico.png"
+    },{
+        game: "The Walking Dead",
+        imag: "http://www.ps3trophies.org/images/trophies/1398/ico.png"
+    }]
 // },{
     // listName: "",
-    // award: "",
+    // award: ,
+    // blurb: "",
     // games: [{
         // game: "",
         // imag: ""
