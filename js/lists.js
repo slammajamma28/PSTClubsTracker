ab14de590ea74487905d721739bda67835a3015e
Slamma <slammajamma28@gmail.com>
2020-02-11 21:13:14 -0500

    Updated to Derby

--- js/lists.js
+++ js/lists.js
@@ -328,6 +328,240 @@ var lists = [{
         game: "The Walking Dead",
         imag: "http://www.ps3trophies.org/images/trophies/1398/ico.png"
     }]
+},{
+    listName: "CB's Chaotic Subscriber Club",
+    award: sub_icon,
+    blurb: "The God of War & Uncharted series are without doubt my favorite gaming series' (although I'd struggle to pick one over the other). Demon's Souls was quite possibly the best game I played on PS3 so had to make the list. I'm also a huge Starwars fan :stormtrooper: with The Force Unleashed being (imo) the best of the games released on console. Lastly I get all nostalgic when thinking of R-type, back home down the arcade playing the game for hours. Dimensions is a great remaster of R-type I & II (the trophy guide writer is also a bit of a legend ;)) :lightsaber:.",
+    games: [{
+        game: "Demon's Souls",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR01249_00_631CC7BC114ED292B852DDE559F6E35D8911BEB6/AE997523EA0B9BA3C26A5C2C895A9914E1171A82.PNG"
+    },{
+        game: "God of War III",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00801_00_4B4C5D6628DF5F38F95120ED06CA733081D44F4C/85C73ACA02B3425A38A428F255F60C5ACABB6C2A.PNG"
+    },{
+        game: "R-Type Dimensions",
+        imag: "https://www.playstationtrophies.org/images/trophies/2399/ico.png"
+    },{
+        game: "Star Wars: The Force Unleashed",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00156_00_BA2858623E381063F20B0F3A5171ACCDC7E0EF3E/5B37C5E22D7306E0593CD25C4BC106CA006345C4.PNG"
+    },{
+        game: "Uncharted: Drake's Fortune",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00449_00_924E0C4EDAD265469D550C987B195F3B74DDED3A/B0C5F9033C9555B3F9E5CBDBF00CD1CD282F9379.PNG"
+    }]
+},{
+    listName: "chrisheadshot's Conveniently Casual Subscriber Club",
+    award: sub_icon,
+    blurb: "Each of these games represent some of my favourite experiences on the Playstation 3. Whether it be because of an amazing online experience, an engaging story or a massive nostalgia kick, each of these games deserves to be a part of your collection.",
+    games: [{
+        game: "Burnout Paradise",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00234_00_B8969C5B3F3361BC2680E954A77FF79D8403B315/04238E4933396903ED8F1F6275AFF6069B3AA056.PNG"
+    },{
+        game: "Call of Duty: Modern Warfare 2",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00791_00_44468E11DB1E6DC3F7AC4728ED03C1278F8F23E8/2DA80D908EDA1F75165EB6402C88D5BAD82B0E6B.PNG"
+    },{
+        game: "Far Cry 3: Blood Dragon",
+        imag: "http://www.ps3trophies.org/images/trophies/2000/ico.png"
+    },{
+        game: "Ratchet & Clank: Going Commando HD",
+        imag: "http://www.ps3trophies.org/images/trophies/1454/ico.png"
+    },{
+        game: "The Walking Dead: Season Two",
+        imag: "http://www.ps3trophies.org/images/trophies/2352/ico.png"
+    }]
+},{
+    listName: "Clayman's History of Violence",
+    award: motm_icon,
+    blurb: "I chose these games because no matter the era, no matter the state of the world or universe, war is always at the forefront unfortunately.",
+    games: [{
+        game: "Battlefield 1",
+        imag: "https://www.playstationtrophies.org/images/trophies/4600/ico.png"
+    },{
+        game: "Call of Duty: World at War",
+        imag: "https://i.imgur.com/KuTXRv8.png"
+    },{
+        game: "Destiny",
+        imag: "https://www.playstationtrophies.org/images/trophies/1542/ico.png"
+    },{
+        game: "Medal of Honor",
+        imag: "https://i.imgur.com/wEiZ85C.png"
+    },{
+        game: "Titanfall 2",
+        imag: "https://www.playstationtrophies.org/images/trophies/4305/ico.png"
+    }]
+},{
+    listName: "clayser's Alternate Ending Subscriber Club",
+    award: sub_icon,
+    blurb: "What these great games all have in common is that they offer different endings and outcomes depending on the choices the player makes while playing.",
+    games: [{
+        game: "Beyond: Two Souls",
+        imag: "https://www.playstationtrophies.org/images/trophies/1585/ico.png"
+    },{
+        game: "Heavy Rain",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00324_00_329289A42218A5976C0B7382FE2B776D4AD99105/28C88C64592408983367C63E71C19BAF64676C6D.PNG"
+    },{
+        game: "inFamous 2",
+        imag: "https://www.playstationtrophies.org/images/trophies/571/ico.png"
+    },{
+        game: "Mass Effect 3",
+        imag: "https://www.playstationtrophies.org/images/trophies/845/ico.png"
+    },{
+        game: "The Walking Dead: Season Two",
+        imag: "https://www.playstationtrophies.org/images/trophies/2352/ico.png"
+    }]
+},{
+    listName: "Combatting's PS4 Exclusive Only Subscriber Club",
+    award: sub_icon,
+    blurb: "Looking to start up on the PS4 with some decent exclusives? Feast up on these.",
+    games: [{
+        game: "Driveclub",
+        imag: "http://www.ps3trophies.org/images/trophies/1933/ico.png"
+    },{
+        game: "inFamous: First Light",
+        imag: "https://www.playstationtrophies.org/images/trophies/2878/ico.png"
+    },{
+        game: "inFamous: Second Son",
+        imag: "https://www.playstationtrophies.org/images/trophies/1936/ico.png"
+    },{
+        game: "The Order: 1886",
+        imag: "https://www.playstationtrophies.org/images/trophies/2104/ico.png"
+    },{
+        game: "Until Dawn",
+        imag: "https://www.playstationtrophies.org/images/trophies/1672/ico.png"
+    }]
+},{
+    listName: "Coop's Crime Obstruction Or Prevention Subscriber Club",
+    award: sub_icon,
+    blurb: "It's up to you to clean up the virtual streets.",
+    games: [{
+        game: "Batman: Arkham City",
+        imag: "http://www.ps3trophies.org/images/trophies/398/ico.png"
+    },{
+        game: "Blue Toad Murder Files: The Mysteries of Little Riddle",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00929_00_EFFFCA104554011E8679BB4CCAACD9CBBCF53B17/ADF485D7B82E48DE1613F13A3157D5EDFA90FB60.PNG"
+    },{
+        game: "L.A. Noire",
+        imag: "http://www.ps3trophies.org/images/trophies/358/ico.png"
+    },{
+        game: "Sleeping Dogs",
+        imag: "http://www.ps3trophies.org/images/trophies/1378/ico.png"
+    },{
+        game: "Watch Dogs",
+        imag: "https://www.playstationtrophies.org/images/trophies/2250/ico.png"
+    }]
+},{
+    listName: "Cyber's Method to the Madness Subscriber Club",
+    award: sub_icon,
+    blurb: "Everyone loves a crazy character.",
+    games: [{
+        game: "Borderlands 2",
+        imag: "http://www.ps3trophies.org/images/trophies/1145/ico.png"
+    },{
+        game: "Diablo III: Reaper of Souls",
+        imag: "https://www.playstationtrophies.org/images/trophies/2253/ico.png"
+    },{
+        game: "Far Cry 3",
+        imag: "http://www.ps3trophies.org/images/trophies/922/ico.png"
+    },{
+        game: "Grand Theft Auto V",
+        imag: "http://www.ps3trophies.org/images/trophies/687/ico.png"
+    },{
+        game: "Red Dead Redemption",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00867_00_3C04BB5C8E8B922C223411F0E040983D86D4B864/27D02EB3579FF13B05CB8707B7B6C79AD14C6332.PNG"
+    }]
+},{
+    listName: "Dark Griever's Cool Chaotic Mayhem Subscriber Club",
+    award: sub_icon,
+    blurb: "I choose these games, because delivering amounts of chaos while saving the day is fun and cool.",
+    games: [{
+        game: "DmC: Devil May Cry Definitive Edition",
+        imag: "https://www.playstationtrophies.org/images/trophies/3309/ico.png"
+    },{
+        game: "Saints Row: The Third",
+        imag: "http://www.ps3trophies.org/images/trophies/746/ico.png"
+    },{
+        game: "Saints Row IV",
+        imag: "https://www.playstationtrophies.org/images/trophies/3031/ico.png"
+    },{
+        game: "Saints Row: Gat Out of Hell",
+        imag: "https://www.playstationtrophies.org/images/trophies/3029/ico.png"
+    }]
+},{
+    listName: " DarkDante's Through Distant Worlds and Times MotM Club",
+    award: motm_icon,
+    blurb: "To be part of this club, explore various open worlds in different times and experience things from both sides of the law.",
+    games: [{
+        game: "Assassin's Creed II",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00894_00_8078706A2F05A74D398BED1ACE3509FAC5B29C89/53046374486E2F6EA6329B78BE7D42070FD576CC.PNG"
+    },{
+        game: "L.A. Noire",
+        imag: "http://www.ps3trophies.org/images/trophies/358/ico.png"
+    },{
+        game: "Mafia II",
+        imag: "http://trophy01.np.community.playstation.net/trophy/np/NPWR00772_00_00FDA376EA4681FF9DBAF192B11C345E528844F6/001EA3716009B7C9AAD4C803F33CACDC831F8200.PNG"
+    },{
+        game: "Saints Row: The Third",
+        imag: "http://www.ps3trophies.org/images/trophies/746/ico.png"
+    },{
+        game: "Sleeping Dogs",
+        imag: "http://www.ps3trophies.org/images/trophies/1378/ico.png"
+    }]
+},{
+    listName: "Deceiver's Destroyer of Dragons Subscriber Club",
+    award: sub_icon,
+    blurb: "I'm a big fan of RPG's and Dragons so why not put the two together.",
+    games: [{
+        game: "Dark Souls",
+        imag: "http://www.ps3trophies.org/images/trophies/755/ico.png"
+    },{
+        game: "Dragon Age: Inquisition",
+        imag: "https://www.playstationtrophies.org/images/trophies/2254/ico.png"
+    },{
+        game: "Dragon's Dogma",
+        imag: "http://www.ps3trophies.org/images/trophies/994/ico.png"
+    },{
+        game: "The Elder Scrolls V: Skyrim",
+        imag: "http://www.ps3trophies.org/images/trophies/826/ico.png"
+    },{
+        game: "The Witcher 3: Wild Hunt",
+        imag: "https://www.playstationtrophies.org/images/trophies/1904/ico.png"
+    }]
+},{
+    listName: "Deceiver's Uni Appreciation MotM Club",
+    award: motm_icon,
+    blurb: "All these games are from the Neptunia series and are either playable on the Vita or feature a trophy image with Uni, the most adorable character. :p",
+    games: [{
+        game: "Hyperdimension Neptunia Re;Birth1",
+        imag: "https://www.playstationtrophies.org/images/trophies/3004/ico.png"
+    },{
+        game: "Hyperdimension Neptunia Re;Birth2 Sisters Generation",
+        imag: "https://www.playstationtrophies.org/images/trophies/3316/ico.png"
+    },{
+        game: "Hyperdimension Neptunia Re;Birth3 V Generation",
+        imag: "https://www.playstationtrophies.org/images/trophies/3747/ico.png"
+    },{
+        game: "Hyperdimension Neptunia U: Action Unleashed",
+        imag: "https://www.playstationtrophies.org/images/trophies/3642/ico.png"
+    },{
+        game: "Cyberdimension Neptunia: 4 Goddesses Online",
+        imag: "https://www.playstationtrophies.org/images/trophies/6491/ico.png"
+    }]
+},{
+    listName: "Derby's Danish Dynamite Subscriber Club",
+    award: sub_icon,
+    blurb: "I chose these games because I really enjoyed them and games like these are most definitely lacking in these clubs. Enjoy a ride around france on your bike, compete against the very best athletes across a wide range of sports in London, and explore the vast open worlds of the US, Hawaii (yes, I'm aware that Hawaii is technically a part of the US) and Ibiza.",
+    games: [{
+        game: "Tour de France 2012",
+        imag: "http://www.ps3trophies.org/images/trophies/1626/ico.png"
+    },{
+        game: "London 2012",
+        imag: "https://www.playstationtrophies.org/images/trophies/1362/ico.png"
+    },{
+        game: "Test Drive Unlimited 2",
+        imag: "https://www.playstationtrophies.org/images/trophies/423/ico.png"
+    },{
+        game: "The Crew",
+        imag: "https://www.playstationtrophies.org/images/trophies/2100/ico.png"
+    }]
 // },{
     // listName: "",
     // award: ,
